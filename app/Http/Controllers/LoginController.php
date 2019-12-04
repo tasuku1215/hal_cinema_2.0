adb0c74ac7e3a07bd71601ff8d5078fcbf923940
至極 <sigoku14.murasaki@gmail.com>
2019-12-04 16:02:10 +0900

    ログイン機構作成

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $login_id = $request->input('id');
+        $password = $request->input('password');
+        $admins = DB::table('admins')
+            ->where('login_id', $login_id)
+            ->first();
+
+        $validationMsgs = [];
+        $id = "";
+        if (!empty($admins)) {
+            if (password_verify($password, $admins->password)) {
+                $id = $admins->user_id;
+                $status = "success";
+            } else {
+                $status = "error";
+                $validationMsgs['password'] = "パスワードが間違ってます。";
+            }
+        } else {
+            $status = "error";
+            $validationMsgs['id'] = "IDが間違ってます。";
+        }
+
+        return response()->json([
+            'id' => $id,
+            'status' => $status,
+            'validationMsgs' => $validationMsgs
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $session = $request->session();
+        $session->flush();
+        $session->regenerate();
+        return redirect("/");
+    }
+}
---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="ログイン画面です" />
+    <!-- Title -->
+    <meta name="keywords" content="" />
+    <!-- StyleSheet -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>ログイン</title>
+</head>
+
+<body id="index" class="index">
+    <!-- header -->
+    <header>
+        <h1>Login</h1>
+    </header>
+    <!-- /header -->
+
+    <!-- main -->
+    <main>
+        <article>
+            <h2>管理者画面ログイン</h2>
+            <div>
+                <p>ログインID：</p>
+                <input type="text" name="login_id" id="login_id">
+                <br>
+                <p>パスワード：</p>
+                <input type="password" name="password" id="password">
+                <button type="submit" name="submit" id="btn">ログイン</button>
+            </div>
+        </article>
+    </main>
+    <!-- /main -->
+
+    <!-- footer -->
+    <footer>
+        <div id="footer">
+            <p id="copyright">&copy;2019 foo</p>
+        </div>
+    </footer>
+    <!-- /footer -->
+</body>
+
+</html>
