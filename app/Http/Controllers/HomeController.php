f506a638d5f133dd1d306335f69d789597aaadf3
至極 <sigoku14.murasaki@gmail.com>
2019-12-09 08:41:45 +0900

    ホーム画面HTMLとcss

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,51 @@ class HomeController extends Controller
 {
     public function showHome()
     {
+
+        //おすすめ映画（今は一つだけ）
+        $recommend = DB::table('labels')
+            ->join('movies', 'movies.movie_id', '=', 'labels.movie_id')
+            ->where('labels.lank', '=', 1)
+            ->orderBy('labels.lank', 'asc')
+            ->get();
+
+        //今日の日付
         $today = Carbon::today();
-        return view("home");
+
+        //料金
+        $money = DB::table('prices')
+            ->where('start_day', '>=', $today)
+            ->orderBy('price', 'desc')
+            ->get();
+
+        return view("home")->with([
+            "recommend" => $recommend,
+            "prices" => $money
+        ]);
+    }
+
+    public function today()
+    {
+        $dt = Carbon::now();
+        $date = $dt->format('m月d日');
+
+        $week = new \App\Libs\DayOfWeek;
+
+        $DoW = $week->dow($dt->dayOfWeek);
+        //初回進入時、当日分表示
+        $today = Carbon::today();
+        $until = Carbon::tomorrow();
+        $schedule = DB::table('shows')
+            ->join('movies', 'movies.movie_id', '=', 'shows.movie_id')
+            ->where('shows.start_datetime', '>=', $today)
+            ->where('shows.end_datetime', '<', $until)
+            ->get();
+
+        return response()->json([
+            'schedule' => $schedule,
+            'date' => $date,
+            'dow' => $DoW,
+            'to' => $today,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,71 +7,93 @@
 @section('content')
 <div id="container">
     <div id="slider">
+        @foreach ($recommend as $rec)
         <section class="s-wrap">
             <div class="box">
-                <!-- <img src="./img/thesha.jpg" alt="topScroll" class="slide-item"> -->
+                <img src="/hal_cinema_2/public/images/movies/{{$rec->img_path}}" alt="topScroll" class="slide-item">
+            </div>
+            <div class="exp">
+                <div class="switch">　</div>
+                <a href="#">
+                    <h2 class="mini-title">{{$rec->movie_title}}</h2>
+                </a>
+                <p>
+                    {{$rec->catchcopy}}
+                </p>
             </div>
         </section>
+        @endforeach
     </div>
 
-    <section class="calen">
-        <h2>上映カレンダー</h2>
-        <div class="movie jurassicworld2">
-            <h3><a href="">ジュラシックワールド　炎の王国</a></h3>
-            <div class="flexbox">
-                <div>10:15 ~</div>
-            </div>
-        </div>
-        <div class="movie pentagon">
-            <h3><a href="">ペンタゴンズ・ペーパー</a></h3>
-            <div class="flexbox">
-                <div>10:30 ~</div>
-                <div>16:00 ~</div>
-            </div>
-        </div>
-        <div class="movie legend">
-            <h3><a href="">アイ　アム　レジェンド</a></h3>
-            <div class="flexbox">
-                <div>13:15 ~</div>
-                <div>15:30 ~</div>
-            </div>
-        </div>
-        <div class="movie eqo">
-            <h3><a href="">The Equalizer2</a></h3>
-            <div class="flexbox">
-                <div>13:30 ~</div>
+    <section class="calender">
+        <div class="calen">
+            <h2>上映カレンダー</h2>
+            <h3 class="date"></h3>
+            <div class="movie">
+                <h3><a href="">ジュラシックワールド　炎の王国</a></h3>
+                <p class="minute"></p>
+                <div class="flexbox">
+                    <div>10:15 ~</div>
+                </div>
             </div>
         </div>
     </section>
 
-    <section id="money">
-        <h2>料金表</h2>
-        <table>
-            <tr>
-                <th>大人</th>
-                <td>1000円</td>
-            </tr>
-            <tr>
-                <th>学生</th>
-                <td>800円</td>
-            </tr>
-            <tr>
-                <th>子供（〜中学生）</th>
-                <td>500円</td>
-            </tr>
-        </table>
+    <section class="money">
+        <div id="money">
+            <h2>料金表</h2>
+            <table>
+                @foreach ($prices as $price)
+                <tr>
+                    <th>{{$price->price_name}}</th>
+                    <td>{{$price->price}}円</td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
     </section>
 
-    <section class="time">
-        <h2>時間案内</h2>
-        <div class="sche">
-            <div>
-                開館時間<span>10:00</span>
-            </div>
-            <div>
-                閉館時間<span>21:00</span>
+    <section id="time">
+        <div class="time">
+            <h2>時間案内</h2>
+            <div class="sche">
+                <div>
+                    開館時間<span>10:00</span>
+                </div>
+                <div>
+                    閉館時間<span>21:00</span>
+                </div>
             </div>
         </div>
     </section>
 </div>
+
+<script language="JavaScript" type="text/javascript">
+    // $(document).ready(function() {
+    //     $('ul#rotation').slide();
+    // });
+</script>
+
+<script type="text/javascript">
+    $("body").ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "/hal_cinema_2/public/api/today",
+            contentType: "Content-Type: application/json; charset=UTF-8",
+        }).done(function(msg, status, xhr) {
+            //success
+            console.log(msg);
+            var i = 0;
+            $(".date").append(msg.date + " （" + msg.dow + "）");
+            for (const [key, value] of Object.entries(msg.schedule)) {
+                $(".minute").append("[上映時間：" + value.screen_time + "min]");
+                i++;
+            }
+        }).fail(function(xhr, status, error) {
+            //error
+            console.log(status);
+        })
+    })
+</script>
+
 @endsection
