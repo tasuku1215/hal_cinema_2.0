5c07626032b3cb42097b1ce007cdce6f2526ab2c
ohs70007 <teardrop0529@docomo.ne.jp>
2019-12-12 17:50:36 +0900

    映画テーブルシーダー編集、料金テーブルシーダー生成

---
+++ database/seeds/PricesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PricesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('prices')->insert([
+            [
+                'price_id' => '1',
+                'price_name' => '大人',
+                'price' => '1000',
+                'start_day' => '2019-12-11',
+                'end_day' => '',
+            ],
+            [
+                'price_id' => '2',
+                'price_name' => '学生',
+                'price' => '800',
+                'start_day' => '2019-12-11',
+                'end_day' => '',
+            ],
+            [
+                'price_id' => '3',
+                'price_name' => '子ども',
+                'price' => '500',
+                'start_day' => '2019-12-11',
+                'end_day' => '',
+            ]
+        ]);
+    }
+}
