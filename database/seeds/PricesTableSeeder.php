1e741b483a64dced0df0b73be5910b54ca8bec7d
ohs70007 <teardrop0529@docomo.ne.jp>
2019-12-13 16:10:15 +0900

    PriceSeeder作り直し

--- database/seeds/PricesTableSeeder.php
+++ database/seeds/PricesTableSeeder.php
@@ -17,21 +17,21 @@ class PricesTableSeeder extends Seeder
                 'price_name' => '大人',
                 'price' => '1000',
                 'start_day' => '2019-12-11',
-                'end_day' => '',
+                'end_day' => '2500-1-1',
             ],
             [
                 'price_id' => '2',
                 'price_name' => '学生',
                 'price' => '800',
                 'start_day' => '2019-12-11',
-                'end_day' => '',
+                'end_day' => '2500-1-1',
             ],
             [
                 'price_id' => '3',
                 'price_name' => '子ども',
                 'price' => '500',
                 'start_day' => '2019-12-11',
-                'end_day' => '',
+                'end_day' => '2500-1-1',
             ]
         ]);
     }
