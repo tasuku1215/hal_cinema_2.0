cccbde527da439094ef5920607fb7eba77e8bb50
tasuku <tasuku1215mhg@gmail.com>
2019-12-17 12:58:38 +0900

    アンケートDBAPI取得

--- resources/views/enquete.blade.php
+++ resources/views/enquete.blade.php
@@ -7,6 +7,8 @@
 @include('tmp/header')
 
 @section('content')
+    {{--    <input type="text" autocomplete="on" list="dt1">--}}
+    {{--    <datalist id="dt1"></datalist>--}}
     <div id="container">
         <section>
             <h2>アンケート</h2>
@@ -42,7 +44,9 @@
                 {{--                <br>--}}
                 <div class="form-group">
                     <label for="">映画タイトル</label>
-                    <input type="text" name="movieName" id="" class="textbox form-control">
+                    <input type="text" name="movieName" id="title" class="textbox form-control" autocomplete="on"
+                           list="dt1">
+                    <datalist id="dt1"></datalist>
                 </div>
                 <div class="form-group">
                     <label for="">アンケート内容</label>
@@ -52,9 +56,9 @@
             </form>
         </section>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-                crossorigin="anonymous"></script>
+        {{--        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"--}}
+        {{--                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"--}}
+        {{--                crossorigin="anonymous"></script>--}}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
                 integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
                 crossorigin="anonymous"></script>
--- resources/views/tmp/header.blade.php
+++ resources/views/tmp/header.blade.php
@@ -22,5 +22,10 @@
 <script type="text/javascript" src="/hal_cinema_2/public/js/slider.js"></script>
 <script type="text/javascript" src="@yield('js')"></script>
 
+{{--<script type="text/javascript" src="/hal_cinema_2/public/js/Jquery.mysuggest.js"></script>--}}
+{{--<link rel="stylesheet" href="/hal_cinema_2/public/css/mysuggest.css" type="text/css">--}}
+<script type="text/javascript" src="/hal_cinema_2/public/js/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="/hal_cinema_2/public/css/jquery-ui.min.css" type="text/css">
+
 <title>@yield('title')</title>
 @endsection
