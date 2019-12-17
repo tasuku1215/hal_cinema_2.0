b3356cc28a41a6d23235d13f75bda7b05c1f4c31
tk-ozawa <t.aroo1512@gmail.com>
2019-12-17 16:46:31 +0900

    デザイン修正

--- resources/views/admin/user/login.blade.php
+++ resources/views/admin/user/login.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta name="description" content="ログイン画面です" />
     <!-- Title -->
     <meta name="keywords" content="" />
@@ -18,32 +19,40 @@
     </header>
     <!-- /header -->
 
-    <!-- main -->
-    <main>
-        <article>
-            <h2>管理者画面ログイン</h2>
-            <div>
-                <form action="/hal_cinema_2/public/admin/user/login" method="post">
-                @csrf
-                    <p>ログインID：</p>
-                    <input type="text" name="login_id" id="login_id">
-                    <br>
-                    <p>パスワード：</p>
-                    <input type="password" name="password" id="password">
-                    <button type="submit" name="submit" id="btn">ログイン</button>
-                </form>
-            </div>
-        </article>
-    </main>
-    <!-- /main -->
+    <div class="container">
+        <!-- main -->
+        <main>
+            <article>
+                <h2>管理者画面ログイン</h2>
+                <div>
+                    <form action="/hal_cinema_2/public/admin/user/login" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="login_id">ログインID：</label>
+                            <input type="text" name="login_id" id="login_id" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">パスワード：</label>
+                            <input type="password" name="password" id="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="submit" class="btn btn-primary">ログイン</button>
+                        </div>
+                    </form>
+                </div>
+            </article>
+        </main>
+        <!-- /main -->
 
-    <!-- footer -->
-    <footer>
-        <div id="footer">
-            <p id="copyright">&copy;2019 foo</p>
-        </div>
-    </footer>
-    <!-- /footer -->
+        <!-- footer -->
+        <footer>
+            <div id="footer">
+                <p id="copyright">&copy;2019 foo</p>
+            </div>
+        </footer>
+        <!-- /footer -->
+    </div>
+
 </body>
 
 </html>
